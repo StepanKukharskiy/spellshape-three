c8333e0e0c518eaf403b3fe625a92023ad3d7ef4
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2025-07-23 20:55:53 +0000

    initial commit

---
+++ modules/interpreter/index.js
@@ -0,0 +1,5 @@
+export { FixedExpressionEvaluator } from './evaluator.js';
+export { FixedConstraintValidator } from './validator.js';
+export { FixedMaterialManager }     from './materials.js';
+export { FixedTemplateProcessor }   from './processor.js';
+export { buildSceneFromSchema }     from './sceneBuilder.js';
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    allowedHosts: [
+      "5173-stepankukha-spellshapet-amqvmw6lpvc.ws-eu120.gitpod.io"
+    ]
+  }
+})
