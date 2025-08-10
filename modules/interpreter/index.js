c225ee46f1f417baff621db95caa1e7dcca6218c
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2025-08-10 11:42:56 +0000

    old modules removed

--- modules/interpreter/index.js
+++
@@ -1,5 +0,0 @@
-export { FixedExpressionEvaluator } from './evaluator.js';
-export { FixedConstraintValidator } from './validator.js';
-export { FixedMaterialManager }     from './materials.js';
-export { FixedTemplateProcessor }   from './processor.js';
-export { buildSceneFromSchema }     from './sceneBuilder.js';
